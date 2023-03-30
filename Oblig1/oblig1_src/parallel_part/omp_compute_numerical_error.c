52b97595b61f372b956bff91d150e5254c722129
Annaaasen <65226318+Annaaasen@users.noreply.github.com>
2023-03-30 08:37:44 +0200

    Parallelization without omp_num_err working

--- Oblig1/oblig1_src/parallel_part/omp_compute_numerical_error.c
+++ Oblig1/oblig1_src/parallel_part/omp_compute_numerical_error.c
@@ -1,8 +1,31 @@
 #include <omp.h>
+#include <math.h>
+#define Pi M_PI
 
 double omp_compute_numerical_error (int nx, int ny, double dx, double dy,
 				    double t_value, double **u)
 {
-  // ...
+
+  double y, x, u_true; 
+
+  int j;
+
+  double pi_t = cos(Pi * t_value);
+  double sum = 0;
+
+#pragma omp parallel for private(y, j, x, u_true) reduction(+:sum)
+  for (int i=0; i<ny-1; i++){
+    y = i*dy;
+    for (int j=0; j<nx-1; j++){
+      x = j*dx;
+      u_true = cos(2*Pi*x) * cos(2*Pi*y) * pi_t; 
+      sum += (u_true - u[i][j])*(u_true - u[i][j]);
+    }
+  }
+
+  double err = sqrt(dx * dy * sum);
+
+  return err;
+
 }
 
--- Oblig1/oblig1_src/serial_part/allocate_2D_array.c
+++ Oblig1/oblig1_src/serial_part/allocate_2D_array.c
@@ -2,13 +2,7 @@
 
 void allocate_2D_array (double ***array_ptr, int nx, int ny)
 {
-  // As we want to change the doulbe pointer array_ptr, we need to send in its pointer
-
-  // *array_ptr = malloc(ny * sizeof(array_ptr));
-  // for (int i=0; i<ny; i++){
-  //   (*array_ptr)[i] = (double*)malloc(nx * sizeof(array_ptr[i]));
-
-  //OR
+  // As we want to change the doulbe pointer array_ptr, we need to send in its pointer@
   *array_ptr = malloc(ny * sizeof(*array_ptr));
   (*array_ptr)[0] = malloc(ny * nx * sizeof((*array_ptr)[0])); //This is a 1D array
   for (int i=1; i<ny; i++){
